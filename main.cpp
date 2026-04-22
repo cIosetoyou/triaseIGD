5f60440038526accf655a9afee5f9c65e5f1a4f8
cIosetoyou <124250130@student.upnyk.ac.id>
2026-04-22 13:21:18 +0700

    add menu

--- main.cpp
+++ main.cpp
@@ -2,8 +2,25 @@
 using namespace std;
 
 int main(){
-	
-    int n;
-    n = 10;
+	int n;
+do{
+    cout<<"=====================================================\n          SISTEM MANAJEMEN TRIASE IGD v1.0          \n=====================================================\n\n";
+	cout<<"-----------------------------------------------------\nMENU UTAMA:\n1. Registrasi Pasien Baru\n2. Panggil Pasien (Proses Penanganan)\n3. Lihat Detail Antrean\n4. Cari Data Pasien\n5. Keluar Program\n-----------------------------------------------------\nPilih menu [1-5]: ";
+	cin>>n; cin.ignore();
+}while (n!=5);
+	switch(n){
+		case 1:{
+
+		}
+		case 2:{
+
+		}
+		case 3:{
+
+		}
+		case 4:{
+
+		}
+	}
 }
 
